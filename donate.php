ffa03a18f9fd88e160dac1faaa86d9903c161f3b
Collen Saunders <collensaunders@icloud.com>
2026-08-14 09:44:48 -0400

    Update donate.php

--- donate.php
+++ donate.php
@@ -3,8 +3,7 @@
 </head>
 <body>
     <main class="donate-container">
-        <h2>Support Pinternext</h2>
-
+        <h2>Support Binternet</h2>
         <div class="qr-box">
           <div class="inner-wrap">
             <h3>Monero [XMR]</h3>
@@ -19,7 +18,7 @@
             alt="xmr qr code (ahwx)"
           />
         </div>
-          
+
         <div class="flex-row">
           <a href="https://ko-fi.com/Ahwxorg" target="_blank" rel="noopener noreferrer"
             ><img
@@ -28,7 +27,6 @@
               height="50"
               width="auto"
           /></a>
-
           <a href="https://www.buymeacoffee.com/ahwx" target="_blank" rel="noopener noreferrer">
             <img
               src="/static/img/buy-me-a-coffee.png"
@@ -37,6 +35,20 @@
               alt="Buy Me a Coffee"
           /></a>
         </div>
+
+        <h2>Support Pinternext</h2>
+        <div class="flex-row">
+          <a href="https://ko-fi.com/cjscloud" target="_blank" rel="noopener noreferrer"
+            ><img
+              src="/static/img/kofi.png"
+              alt="Ko-fi"
+              height="50"
+              width="auto"
+          /></a>
+        </div>
+    </main>
+</body>
+</html>
     </main>
 
 <?php require "misc/footer.php"; ?>
